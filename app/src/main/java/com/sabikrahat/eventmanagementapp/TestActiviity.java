e30eaa774d092a7e1ffe7e70e29a07affac8a2aa
Payal Gupta <payal.newspark20@gmail.com>
2025-10-23 16:01:08 +0530

    date- 23 Oct 2025 changes- TestActivity added

---
+++ app/src/main/java/com/sabikrahat/eventmanagementapp/TestActiviity.java
@@ -0,0 +1,14 @@
+package com.sabikrahat.eventmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActiviity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_activiity);
+    }
+}
