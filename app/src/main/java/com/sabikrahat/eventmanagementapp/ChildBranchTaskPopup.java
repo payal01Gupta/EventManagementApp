220e08e6bc116fe1859efbd372491f752163ffcd
Payal Gupta <payal.newspark20@gmail.com>
2025-10-24 16:29:45 +0530

    Stop trackinfg testgitignore folder

---
+++ app/src/main/java/com/sabikrahat/eventmanagementapp/ChildBranchTaskPopup.java
@@ -0,0 +1,14 @@
+package com.sabikrahat.eventmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChildBranchTaskPopup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_branch_task_popup);
+    }
+}
