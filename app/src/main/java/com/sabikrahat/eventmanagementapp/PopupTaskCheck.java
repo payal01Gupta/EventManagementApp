3d6829d23533e8570250722746b3c31df7662886
Payal Gupta <payal.newspark20@gmail.com>
2025-10-24 15:23:56 +0530

    date- 24 Oct 2025 changes- PopupTaskCheck Activity added

---
+++ app/src/main/java/com/sabikrahat/eventmanagementapp/PopupTaskCheck.java
@@ -0,0 +1,14 @@
+package com.sabikrahat.eventmanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopupTaskCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup_task_check);
+    }
+}
