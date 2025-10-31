0d44998d6cd74546541a0c1053a1c71ad11a6b4a
Payal Gupta <payal.newspark20@gmail.com>
2025-10-31 11:59:44 +0530

    MEthod added in dummy activity

--- app/src/main/java/com/sabikrahat/eventmanagementapp/DummyActivity.java
+++ app/src/main/java/com/sabikrahat/eventmanagementapp/DummyActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DummyActivity extends AppCompatActivity {
     TextView textView;
@@ -16,6 +17,11 @@ public class DummyActivity extends AppCompatActivity {
         textView.setText("Payal Gupta");
 
         Log.e("payal","dummy");
+        testDummy();
+    }
+
+    private void testDummy() {
+        Toast.makeText(this, "dummy method", Toast.LENGTH_SHORT).show();
     }
 
 
